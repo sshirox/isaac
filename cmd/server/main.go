b7ac6bd03bb2030b1b18d5b6019dabd237829b58
sshirox <sshirox@yandex.ru>
2025-01-14 21:48:50 +0500

    Iter20: Add build info

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"github.com/sshirox/isaac/internal/server"
 )
 
+var (
+	buildVersion = "N/A"
+	buildDate    = "N/A"
+	buildCommit  = "N/A"
+)
+
 func main() {
+	fmt.Printf("Build version: %s\nBuild date: %s\nBuild commit: %s\n", buildVersion, buildDate, buildCommit)
+
 	if err := server.Run(); err != nil {
 		panic(err)
 	}
